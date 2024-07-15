278d3877f5c7dbd6ee39abcc1fa22a45d5667425
Gunheer Kaur <gunheerahuja@gmail.com>
2024-07-15 20:04:03 +0530

    Updated project with new features

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import ContinueButton from './components/ContinueButton';
 import VirtualTryOn from './components/VirtualTryOn';
 import UserProfile from './components/UserProfile';
 import Wardrobe from './components/Wardrobe';
+import pinktop from './components/pink top.jpg';
+import top from './components/top.png'
+import blacktop from './components/blacktop.jpg';
 import './styles.css';
 import './App.css';
 
@@ -14,12 +17,12 @@ const App = () => {
   const [selectedItem, setSelectedItem] = useState(null);
   const [user] = useState({ name: 'John Doe', email: 'john.doe@example.com' });
   const [wardrobe] = useState([
-    { id: 1, name: 'T-Shirt', image: 'https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3JvYl9yYXdwaXhlbF93aGl0ZV90X3NoaXJ0X21vY2t1cF93aGl0ZV9iYWNrZ3JvdW5kXzY5MWZiNDdjLTY5NzYtNGEyMS05MzU2LTNlY2U0NDNjOTUyMi5wbmc.png', model: 'path/to/tshirt.gltf' },
+    { id: 1, name: 'Tank tops', image: pinktop, model: 'path/to/tshirt.gltf' },
     { id: 2, name: 'Jeans', image: 'https://e7.pngegg.com/pngimages/830/804/png-clipart-blue-denim-pants-topshop-mom-jeans-denim-slim-fit-pants-jeans-fashion-top-thumbnail.png', model: 'path/to/jeans.gltf' },
-    { id: 3, name: 'Shirt', image: 'https://w7.pngwing.com/pngs/955/626/png-transparent-men-s-blue-dress-shirt-and-blue-and-gray-necktie-t-shirt-dress-shirt-suit-necktie-t-shirt-blue-electric-blue-formal-wear-thumbnail.png', model: 'path/to/tshirt.gltf' },
+    { id: 3, name: 'Formal Wear', image: blacktop, model: 'path/to/tshirt.gltf' },
     { id: 4, name: 'Cardigan', image: 'https://w7.pngwing.com/pngs/602/71/png-transparent-cardigan-beige-wool-kitsch-woolen-cardigan-jacket-thumbnail.png', model: 'path/to/jeans.gltf' },
     { id: 5, name: 'Sweater', image: 'https://static.vecteezy.com/system/resources/previews/036/114/258/non_2x/ai-generated-isolated-sweater-fashion-piece-of-clothing-on-a-transparent-background-file-format-png.png', model: 'path/to/tshirt.gltf' },
-    { id: 6, name: 'Jackets', image: 'https://w7.pngwing.com/pngs/898/68/png-transparent-leather-jacket-flight-jacket-a-2-jacket-sheepskin-jacket-thumbnail.png', model: 'path/to/jeans.gltf' },
+    { id: 6, name: 'Tops', image: top , model: 'path/to/jeans.gltf' },
     
   ]);
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
+// components/Header.js
+
 import React from 'react';
 
 
-const Header = () => {
-  return (
-    <header>
-      <h1>Virtual Wardrobe</h1>
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <img src="https://www.freepnglogos.com/uploads/logo-myntra-png/myntra-Indian-startups-just-can-stop-tinkering-with-their-logos-28.png" alt="Myntra Logo" className="logo" />
+    <h1>Virtual Wardrobe</h1>
+  </header>
+);
 
 export default Header;
